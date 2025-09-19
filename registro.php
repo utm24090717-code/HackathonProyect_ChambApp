cc24311d813dbf5b5453a2454a5e2d6628ac75fa
JoseMRG777 <jose4pcasus@gmail.com>
2025-09-19 14:18:51 -0600

    chidooox

---
+++ registro.php
@@ -0,0 +1,30 @@
+<?php
+
+include("conexion.php");
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre    = $conn->real_escape_string($_POST['nombre']);
+    $apellidos = $conn->real_escape_string($_POST['apellidos']);
+    $correo    = $conn->real_escape_string($_POST['correo']);
+    $telefono  = $conn->real_escape_string($_POST['telefono']);
+    $contraseña  = $conn->real_escape_string($_POST['contraseña']);
+
+    if (!empty($nombre) && !empty($apellidos) && !empty($correo) && !empty($telefono)) {
+        
+        $sql = "INSERT INTO usuarios (nombre, apellidos, correo, telefono) 
+                VALUES ('$nombre', '$apellidos', '$correo', '$telefono')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "✅ Cuenta creada con éxito";
+        } else {
+            echo "❌ Error: " . $conn->error;
+        }
+    } else {
+        echo "⚠️ Todos los campos son obligatorios";
+    }
+}
+
+// Cerrar conexión
+$conn->close();
+?>
