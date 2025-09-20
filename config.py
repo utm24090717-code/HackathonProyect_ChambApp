9a37de658b7f6569477575b93ff6a8ff72d8c730
JoseMRG777 <jose4pcasus@gmail.com>
2025-09-19 21:20:47 -0600

    ww

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",       # cambia por tu usuario
-    "password": "",       # tu password
-    "database": "mi_bd"   # tu base de datos
-} 
